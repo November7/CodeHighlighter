29e9506038a0ceb85d312bdeab97cc5032112ff2
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2024-02-18 23:07:43 +0100

    Fake update

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release   = '2.0';
-$plugin->version   = 2024021803;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2024021804;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'filter_codehighlighter'; // Full name of the plugin (used for diagnostics)
 
